0a258bd759149824062cdb7df37d717e3fcc1032
mohammad ehsan mohit <mohammadehsan.m@780.ir>
2019-07-06 15:58:48 +0430

    Set Validator on SubmitButton

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -20,19 +20,60 @@ class LoginScreenState extends State<LoginScreen> {
         key: _formKey,
         autovalidate: true,
         child: Column(
-          children: <Widget>[
-            emailField(),
-            passWordField(),
-            submitButton()
-          ],
+          children: <Widget>[emailField(), passWordField(), submitButton()],
         ),
       ),
     );
   }
 
-  Widget emailField() {}
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Email Address',
+          hintText: 'you@example.com',
+          icon: Icon(Icons.email, color: Colors.grey)),
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      //validator: (){
+        
+      //},
+    );
+  }
 
-  Widget passWordField() {}
+  Widget passWordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Password',
+          hintText: '8-16 Character',
+          icon: Icon(Icons.vpn_key, color: Colors.grey)),
+      maxLength: 16,
+      maxLines: 1,
+      textInputAction: TextInputAction.done,
+      obscureText: true,
+    );
+  }
 
-  Widget submitButton() {}
+  Widget submitButton() {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: RaisedButton.icon(
+          disabledColor: Colors.grey,
+          color: Colors.blue,
+          onPressed: () {
+            _formKey.currentState.validate();
+          },
+          icon: Icon(
+            Icons.done,
+            size: 40.0,
+            color: Colors.green,
+          ),
+          label: Text(
+            'Submit',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 25.0),
+          )),
+    );
+  }
 }
